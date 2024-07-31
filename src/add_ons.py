776a50ec5000637f43036a0d3f4f7788f041200d
Beverly Xu <xu21beve@gmail.com>
2024-07-31 06:10:15 +0100

    logic flow and basic recognition functional

--- src/add_ons.py
+++ src/add_ons.py
@@ -23,12 +23,16 @@ from PIL import Image
 from tflite_runtime.interpreter import Interpreter
 
             # name              bin         material             weight
-ADD_ONS = [['compostable-fork', 'compost', 'compostable-plastic', '-1'],
-           ['one-use-fork', 'trash', 'plastic', '-1'],
-           ['one-use-knife', 'trash', 'plastic', '-1'],
-           ['mayo', 'trash', 'plastic', '-1'],
-           ['chili', 'trash', 'plastic', '-1']
-           ['mustard', 'trash', 'plastic', '-1']]
+ADD_ONS = [['mayo', 'trash', 'plastic', 13.2],
+           ['mustard', 'trash', 'plastic', 6.1],
+           ['chili', 'trash', 'plastic', 7.7],
+           ['compostable-fork', 'compost', 'compostable-plastic', 5.25],
+           # ['compostable-spoon', 'compost', 'compostable-plastic', -1],
+           ['one-use-fork', 'trash', 'plastic', -1]]
+           # ['one-use-knife', 'trash', 'plastic', '-1'],
+           # ['napkin', 'compost, recycling', 'unwaxed-paper', '-1'],
+           # ]
+
 # ADD_ONS = [['compostable-fork', 'compost', 'compostable-plastic', '-1'],
 #            ['compostable-spoon', 'compost', 'compostable-plastic', '-1'],
 #            ['one-use-fork', 'trash', 'plastic', '-1'],
@@ -43,68 +47,21 @@ def load_labels(filename):
   with open(filename, 'r') as f:
     return [line.strip() for line in f.readlines()]
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '-i',
-      '--image',
-      default='../06:31:44.jpg',
-      help='image to be classified')
-  parser.add_argument(
-      '-m',
-      '--model_file',
-      default='../model_unquant.tflite',
-      help='.tflite model to be executed')
-  parser.add_argument(
-      '-l',
-      '--label_file',
-      default='../labels.txt',
-      help='name of file containing labels')
-  parser.add_argument(
-      '--input_mean',
-      default=127.5, type=float,
-      help='input_mean')
-  parser.add_argument(
-      '--input_std',
-      default=127.5, type=float,
-      help='input standard deviation')
-  parser.add_argument(
-      '--num_threads', default=None, type=int, help='number of threads')
-  parser.add_argument(
-      '-e', '--ext_delegate', help='external_delegate_library path')
-  parser.add_argument(
-      '-o',
-      '--ext_delegate_options',
-      help='external delegate options, \
-            format: "option1: value1; option2: value2"')
-
-  args = parser.parse_args()
-
+def identify_utensils():
+  image_file = '../06:04:39.jpg'
+  model_file = '../add_on_model_unquant.tflite'
+  label_file = '../add_on_labels.txt'
+  input_mean = 127.5
+  input_std = 127.5
+  num_threads = None
+  
   ext_delegate = None
   ext_delegate_options = {}
 
-  # parse extenal delegate options
-  if args.ext_delegate_options is not None:
-    options = args.ext_delegate_options.split(';')
-    for o in options:
-      kv = o.split(':')
-      if (len(kv) == 2):
-        ext_delegate_options[kv[0].strip()] = kv[1].strip()
-      else:
-        raise RuntimeError('Error parsing delegate option: ' + o)
-
-  # load external delegate
-  if args.ext_delegate is not None:
-    print('Loading external delegate from {} with args: {}'.format(
-        args.ext_delegate, ext_delegate_options))
-    ext_delegate = [
-        tflite.load_delegate(args.ext_delegate, ext_delegate_options)
-    ]
-
   interpreter = Interpreter(
-      model_path=args.model_file,
+      model_path=model_file,
       experimental_delegates=ext_delegate,
-      num_threads=args.num_threads)
+      num_threads=None)
   interpreter.allocate_tensors()
 
   input_details = interpreter.get_input_details()
@@ -116,13 +73,13 @@ if __name__ == '__main__':
   # NxHxWxC, H:1, W:2
   height = input_details[0]['shape'][1]
   width = input_details[0]['shape'][2]
-  img = Image.open(args.image).resize((width, height))
+  img = Image.open(image_file).resize((width, height))
 
   # add N dim
   input_data = np.expand_dims(img, axis=0)
 
   if floating_model:
-    input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+    input_data = (np.float32(input_data) - input_mean) / input_std
 
   interpreter.set_tensor(input_details[0]['index'], input_data)
 
@@ -134,7 +91,7 @@ if __name__ == '__main__':
   results = np.squeeze(output_data)
 
   top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(args.label_file)
+  labels = load_labels(label_file)
   object_ids = []
   highest_probability_id = -1
 
@@ -147,3 +104,5 @@ if __name__ == '__main__':
             highest_probability_id = i 
     else:
       print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
+  
+  return object_ids
